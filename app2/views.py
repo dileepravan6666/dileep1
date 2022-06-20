967c0d1c44109145a37896bbc3fcd66d5eb2a4fc
dileepravan6666 <dileepkomarla@gmail.com>
2022-06-20 12:45:52 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def kohli(request):
+    return HttpResponse(' kohli best inspiration to everyone')
+    
